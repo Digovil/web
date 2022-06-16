83fa3c4deaafcb55bb7764d8ac189563c4bfa071
Digovil <dilangvidal@gmail.com>
2022-06-15 19:38:24 -0500

    envio de correo

---
+++ assets/php/sendmail.php
@@ -0,0 +1,16 @@
+<?php
+// 	if(isset($_POST["submit"])) {
+// 		$recipient = 'dilangvidal@gmail.com'; //my email
+// 		$subject = $_POST['subject'];
+// 		$name = $_POST ["name"];
+// 		$email = $_POST["email"];
+// 		$message = $_POST["message"];
+		
+// 		$mailBody="Name: $name\nEmail: $email\n\n$message";
+// 		mail($recipient, $subject, $mailBody, "From: $name <$email>");
+// 		$thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
+// 	}
+// 
+
+mail('dilangvidal@gmail.com', 'My Subject', "HOLA");
+?>
